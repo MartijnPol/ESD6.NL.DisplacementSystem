dd827a6e16f64c182faebb82dddc17d9d33a17ff
Violet Kampers <282404@student.fontys.nl>
2018-03-22 15:10:28 +0100

    Revert "Revert "Added the unit tests for CarTrackerService""
    This reverts commit 0f3d46dd751991cde51e91a56f13eee744ceca55.

---
+++ src/test/java/dao/CarTrackerDaoTest.java
@@ -0,0 +1,27 @@
+package dao;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CarTrackerDaoTest {
+
+    @Before
+    public void setUp(){
+
+    }
+
+    @Test
+    public void getRulesWithinPeriod_CarTrackerIDStartDateEndDate_CarTrackerFound(){
+
+    }
+
+    @Test
+    public void getRulesWithinMultiplePeriods_CarTrackerDataQueryList_ListOfCarTrackersFound(){
+
+    }
+
+    @Test
+    public void getLastRuleId_None_GetLastRuleId(){
+
+    }
+}
---
+++ src/test/java/dao/ProcessedCarsDaoTest.java
@@ -0,0 +1,17 @@
+package dao;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProcessedCarsDaoTest {
+
+    @Before
+    public void setUp(){
+
+    }
+
+    @Test
+    public void getNotProcessedDataById_Id_ListOfNotProcessedCars(){
+
+    }
+}
