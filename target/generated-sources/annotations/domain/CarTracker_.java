646057567178d0a31b6e33cf2f705bfd1191c012
MartijnPol <martijn.pol@hotmail.com>
2018-05-07 14:10:06 +0200

    Central database is working!

--- target/generated-sources/annotations/domain/CarTracker_.java
+++ target/generated-sources/annotations/domain/CarTracker_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-16T11:57:48")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-07T12:43:01")
 @StaticMetamodel(CarTracker.class)
 public class CarTracker_ { 
 
