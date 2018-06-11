67e254bde3f1142624f4131a907c441d2568823f
MvG013 <marcvangool@hotmail.com>
2018-06-11 16:29:25 +0200

    Fixed bug till google api

--- src/main/java/service/StartUp.java
+++ src/main/java/service/StartUp.java
@@ -36,16 +36,14 @@ public class StartUp {
 
     @PostConstruct
     public void initData() {
-        CarTracker carTracker = new CarTracker();
-        carTracker.setId("NLD1");
-        carTracker.setStartDate(new Date());
-        carTracker.setManufacturer("ASUS");
-        carTracker.setEnabled(true);
+//        CarTracker carTracker = new CarTracker();
+//        carTracker.setId("NLD1");
+//        carTracker.setStartDate(new Date());
+//        carTracker.setManufacturer("ASUS");
+//        carTracker.setEnabled(true);
 
 //        List<CarTrackerRule> carTrackerRules = new ArrayList<CarTrackerRule>();
-//        carTrackerRules.add(new CarTrackerRule(carTracker, 2L, new GregorianCalendar(2017, Calendar.DECEMBER, 1).getTime(), 51.560596, 5.091914));
-//        carTrackerRules.add(new CarTrackerRule(carTracker, 3L, new GregorianCalendar(2017, Calendar.DECEMBER, 2).getTime(), 51.523677, 5.064195));
-//        carTrackerRules.add(new CarTrackerRule(carTracker, 4L, new GregorianCalendar(2017, Calendar.DECEMBER, 3).getTime(), 51.523677, 5.064195));
+//        carTrackerRules.add(new CarTrackerRule(carTracker, 151L, new GregorianCalendar(2017, Calendar.DECEMBER, 1).getTime(), 51.560596, 5.091914));
 //        carTracker.setRules(carTrackerRules);
 //
 //        for (CarTrackerRule rule: carTrackerRules)
@@ -53,16 +51,33 @@ public class StartUp {
 //            rule.setCarTracker(carTracker);
 //        }
 //        carTracker.setTotalRules((long) carTrackerRules.size());
-        carTrackerService.create(carTracker);
+//        carTrackerService.create(carTracker);
 //
+//        CarTracker carTracker1 = new CarTracker();
+//        carTracker1.setId("NLD2");
+//        carTracker1.setStartDate(new Date());
+//        carTracker1.setManufacturer("Apple");
+//        carTracker1.setEnabled(true);
+//
+//        List<CarTrackerRule> carTrackerRules1 = new ArrayList<CarTrackerRule>();
+//        carTrackerRules1.add(new CarTrackerRule(carTracker1, 151L, new GregorianCalendar(2018, Calendar.JUNE, 11).getTime(), 51.52322, 5.06485));
+//        carTracker1.setRules(carTrackerRules1);
+//
+//        for (CarTrackerRule rule: carTrackerRules1)
+//        {
+//            rule.setCarTracker(carTracker1);
+//        }
+//        carTracker1.setTotalRules((long) carTrackerRules1.size());
+//        carTrackerService.create(carTracker1);
+////
 //        try {
 //            messageProducer.sendMessage(jsonReader.readJsonFiles());
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
 
-        Credentials credentials = new Credentials("admin");
-        credentials.setToken("fmrh7fpci1m6t53618b94iqr0d");
-        authenticationService.createCredentials(credentials);
+//        Credentials credentials = new Credentials("admin");
+//        credentials.setToken("fmrh7fpci1m6t53618b94iqr0d");
+//        authenticationService.createCredentials(credentials);
     }
 }
