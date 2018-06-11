83ce5e1c2718349f2873d46cf36879c8ff695478
MvG013 <marcvangool@hotmail.com>
2018-06-11 15:33:48 +0200

    Changed toJson metersDriven

--- src/main/java/service/StartUp.java
+++ src/main/java/service/StartUp.java
@@ -42,24 +42,24 @@ public class StartUp {
         carTracker.setManufacturer("ASUS");
         carTracker.setEnabled(true);
 
-        List<CarTrackerRule> carTrackerRules = new ArrayList<CarTrackerRule>();
-        carTrackerRules.add(new CarTrackerRule(carTracker, 2L, new GregorianCalendar(2017, Calendar.DECEMBER, 1).getTime(), 51.560596, 5.091914));
-        carTrackerRules.add(new CarTrackerRule(carTracker, 3L, new GregorianCalendar(2017, Calendar.DECEMBER, 2).getTime(), 51.523677, 5.064195));
-        carTrackerRules.add(new CarTrackerRule(carTracker, 4L, new GregorianCalendar(2017, Calendar.DECEMBER, 3).getTime(), 51.523677, 5.064195));
-        carTracker.setRules(carTrackerRules);
-
-        for (CarTrackerRule rule: carTrackerRules)
-        {
-            rule.setCarTracker(carTracker);
-        }
-        carTracker.setTotalRules((long) carTrackerRules.size());
+//        List<CarTrackerRule> carTrackerRules = new ArrayList<CarTrackerRule>();
+//        carTrackerRules.add(new CarTrackerRule(carTracker, 2L, new GregorianCalendar(2017, Calendar.DECEMBER, 1).getTime(), 51.560596, 5.091914));
+//        carTrackerRules.add(new CarTrackerRule(carTracker, 3L, new GregorianCalendar(2017, Calendar.DECEMBER, 2).getTime(), 51.523677, 5.064195));
+//        carTrackerRules.add(new CarTrackerRule(carTracker, 4L, new GregorianCalendar(2017, Calendar.DECEMBER, 3).getTime(), 51.523677, 5.064195));
+//        carTracker.setRules(carTrackerRules);
+//
+//        for (CarTrackerRule rule: carTrackerRules)
+//        {
+//            rule.setCarTracker(carTracker);
+//        }
+//        carTracker.setTotalRules((long) carTrackerRules.size());
         carTrackerService.create(carTracker);
-
-        try {
-            messageProducer.sendMessage(jsonReader.readJsonFiles());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//
+//        try {
+//            messageProducer.sendMessage(jsonReader.readJsonFiles());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
         Credentials credentials = new Credentials("admin");
         credentials.setToken("fmrh7fpci1m6t53618b94iqr0d");
